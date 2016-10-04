be62e185612132e43bce88f93dc8e0a8db3547f1
Deann Huang <DeannHuang@kurotimes.com.tw>
2016-10-04 17:45:33 +0800

    commit

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    	<div>
+    		<h1>hello, world</h1>
+    	</div>
+    );
+  }
+}
+//stateless
+ReactDOM.render(<App />,document.getElementById('app'));
+
+const MyComponent = () =>(
+			<h1>hello~~ world!</h1>
+)
+
+ReactDOM.render(<MyComponent />,document.getElementById('justSayHello'));
